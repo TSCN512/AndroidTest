207708d4217d2cd147affb26041866cac2555d33
TSCN <franciscoubaldo512@gmail.com>
2018-03-20 01:59:07 -0400

    Started to use button to start a new activity from main activity, also added the jsoup library

--- app/src/main/java/com/example/owner/androidtest/MainActivity.java
+++ app/src/main/java/com/example/owner/androidtest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.owner.androidtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.ib);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NextActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
