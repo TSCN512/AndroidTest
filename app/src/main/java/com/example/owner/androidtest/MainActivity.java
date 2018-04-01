409cf986cab0146c7c47e668d05366b89a36dd58
TSCN <franciscoubaldo512@gmail.com>
2018-04-01 05:36:09 -0400

    Started to use button to start a new activity from main activity, also added the jsoup library

--- app/src/main/java/com/example/owner/androidtest/MainActivity.java
+++ app/src/main/java/com/example/owner/androidtest/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.owner.androidtest;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -11,8 +14,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+
         ImageView imageView = (ImageView) findViewById(R.id.ib);
+        //imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        imageView.setAdjustViewBounds(true);
+        /*Display display = getWindowManager().getDefaultDisplay();
+        Point myPoint = new Point();
+        display.getSize(myPoint);
+
+
+        imageView.setMinimumWidth(myPoint.x);
+        imageView.setMinimumHeight(myPoint.y);
+
+        imageView.setMaxWidth(myPoint.x);
+        imageView.setMaxHeight(myPoint.y);*/
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
