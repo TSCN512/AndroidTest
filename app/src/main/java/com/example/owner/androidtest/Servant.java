604af5dd22a7a9533f482412176d14d5f6490c50
TSCN <franciscoubaldo512@gmail.com>
2018-04-14 23:44:48 -0400

    AsyncTask still recommended, cleaned up output a little Reorganized structure, only scraps after checking NA servant ID

--- app/src/main/java/com/example/owner/androidtest/Servant.java
+++ app/src/main/java/com/example/owner/androidtest/Servant.java
@@ -16,77 +16,15 @@ public class Servant
 
     public Servant(int ID)
     {
-        String[] myArray = new String[16];
+        String[] myArray = {"Name: ","Alias: ","Class: ", "ID: ", "Rarity: ", "Cost: ", "Max Level Cap: ", "Starting ATK: ", "Starting HP: ", "Max Level ATK: ", "Max Level HP: ", "","","",""};
         //myArray[0] = "READY";
         try {
             //we will concatenate the servant id part of the wiki after reading user input
             int x = ID; //servant id
-            String webpage = "https://fate-go.cirnopedia.org/servant_profile.php?servant=" + x;
-            String myQuery;
-            switch (x)  //this switch statement accommodates the different webpage layouts
-            {
-                case 5: {
-                    myQuery = "#content > a:nth-child(14) > div:nth-child(1) > table:nth-child(1)";
-                    break;
-                }
-                case 94: {
-                    myQuery = "#content > a:nth-child(14) > div:nth-child(1) > table:nth-child(1)";
-                    break;
-                }
-                case 99: {
-                    myQuery = "#content > a:nth-child(14) > div:nth-child(1) > table:nth-child(1)";
-                    break;
-                }
-                case 106: {
-                    myQuery = "#content > a:nth-child(14) > div:nth-child(1) > table:nth-child(1)";
-                    break;
-                }
-                case 183: {
-                    myQuery = "#content > a:nth-child(13) > div:nth-child(1) > table:nth-child(1)";
-                    break;
-                }
-                default: {
-                    if (x < 161)
-                        myQuery = "#content > a:nth-child(13) > div:nth-child(1) > table:nth-child(1)";
-                    else
-                        myQuery = "#content > a:nth-child(12) > div:nth-child(1) > table:nth-child(1)";
-                    break;
-                }
-            }
-
-
-            Document doc = Jsoup.connect(webpage).get();
-            String title = doc.title();
-            System.out.println(title);
-            Elements links = doc.select(myQuery);
-            String atk = links.toString();//.substring(links.toString().indexOf("ATK Lv.1"));
-            String atk2 = links.toString();
-            if(!atk2.contains("★★★★★") && !atk2.contains("★★★★") && !atk2.contains("★★★"))
-            {
-                if (atk2.contains("★★"))
-                    atk2 = atk2.replace("★★", "★★ UC");
-                else if (atk2.contains("★"))
-                    atk2 = atk2.replace("★", "★ C");
-            }
-
-            atk2 = atk2.replaceAll("<[^>]*>", "").replaceAll("\n[^a-zA-Z_0-9｢?]+\n", "\n"); //servant 91 has a unique name beginning with ｢
-            atk2 = atk2.replaceFirst("\n.*\n", "\n");
-            atk2 = atk2.replaceAll("\n.*\n", "\n");
-            atk2 = atk2.replaceFirst("\n", "");
-
-            myArray = atk2.split("\n");
-            for (int j = 0; j < myArray.length; j++)
-            {
-                if (x == 1 && j == 1) //Mash has fused with an unknown heroic spirit, true name unknown
-                    myArray[j] = "TRUE NAME UNKNOWN";
-                else
-                    myArray[j] = myArray[j].trim();
-            }
-            //System.out.println(myArray[3]);
             if (x == 83)  //SERVANT ID 83, 149, 151, 152 HAVE INCOMPATIBLE SPIRIT ORIGINS
             {
                 special = true;
-                setSpecialMsg("CLASS: GRAND CASTER  \nNAME: KING SOLOMON, KING OF MAGES");
+                setSpecialMsg("CLASS: GRAND CASTER  \nNAME: KING SOLOMON, KING OF MAGES\nREMAINING SPIRIT GRAPH DATA INCOMPATIBLE WITH CHALDEA PROTOCOLS");
             }
             else if (x > 106) // NA known list only includes up to 106
             {
@@ -95,6 +33,76 @@ public class Servant
             }
             else
             {
+                String webpage = "https://fate-go.cirnopedia.org/servant_profile.php?servant=" + x;
+                String myQuery;
+                switch (x)  //this switch statement accommodates the different webpage layouts
+                {
+                    case 1: {
+                        myQuery = "#content > a:nth-child(14) > div:nth-child(1) > table:nth-child(1)";
+                        break;
+                    }
+                    case 5: {
+                        myQuery = "#content > a:nth-child(14) > div:nth-child(1) > table:nth-child(1)";
+                        break;
+                    }
+                    case 94: {
+                        myQuery = "#content > a:nth-child(14) > div:nth-child(1) > table:nth-child(1)";
+                        break;
+                    }
+                    case 99: {
+                        myQuery = "#content > a:nth-child(14) > div:nth-child(1) > table:nth-child(1)";
+                        break;
+                    }
+                    case 106: {
+                        myQuery = "#content > a:nth-child(14) > div:nth-child(1) > table:nth-child(1)";
+                        break;
+                    }
+                    case 183: {
+                        myQuery = "#content > a:nth-child(13) > div:nth-child(1) > table:nth-child(1)";
+                        break;
+                    }
+                    default: {
+                        if (x < 161)
+                            myQuery = "#content > a:nth-child(13) > div:nth-child(1) > table:nth-child(1)";
+                        else
+                            myQuery = "#content > a:nth-child(12) > div:nth-child(1) > table:nth-child(1)";
+                        break;
+                    }
+                }
+
+
+                Document doc = Jsoup.connect(webpage).get();
+                String title = doc.title();
+                System.out.println(title);
+                Elements links = doc.select(myQuery);
+                Elements link2 = doc.select(".status > tbody:nth-child(2) > tr:nth-child(8) > td:nth-child(2)");
+                String traits = link2.toString().replaceAll("<[^>]*>","");
+                setTraits(traits);
+                String atk = links.toString();//.substring(links.toString().indexOf("ATK Lv.1"));
+                String atk2 = links.toString();
+                if(!atk2.contains("★★★★★") && !atk2.contains("★★★★") && !atk2.contains("★★★"))
+                {
+                    if (atk2.contains("★★"))
+                        atk2 = atk2.replace("★★", "★★ UC");
+                    else if (atk2.contains("★"))
+                        atk2 = atk2.replace("★", "★ C");
+                }
+
+                atk2 = atk2.replaceAll("<[^>]*>", "").replaceAll("\n[^a-zA-Z_0-9｢?]+\n", "\n"); //servant 91 has a unique name beginning with ｢
+                atk2 = atk2.replaceFirst("\n.*\n", "\n");
+                atk2 = atk2.replaceAll("\n.*\n", "\n");
+                atk2 = atk2.replaceFirst("\n", "");
+
+                String myArray2[] = atk2.split("\n");
+                for (int j = 0; j < myArray.length; j++)
+                {
+                    if (x == 1 && j == 1) //Mash has fused with an unknown heroic spirit, true name unknown
+                        myArray[j] = "TRUE NAME UNKNOWN";
+                    else
+                        myArray[j] += myArray2[j].trim();
+                }
+                //System.out.println(myArray[3]);
+
                 stats=myArray;
             }
 
@@ -126,6 +134,7 @@ public class Servant
             String info = "";
             for (String value : stats)
                 info += value + '\n';
+            info+= traits;
             info = info.trim();
             return info;
         }
