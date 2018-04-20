cfc1fd5ae5a5386bcba1eb9afc78fff7ca4f3274
TSCN <franciscoubaldo512@gmail.com>
2018-04-19 23:24:48 -0400

    AsyncTask still recommended on Servant.java, but implemented on portrait.java attempted a portrait download on portrait.java, fully tested, works

--- app/src/main/java/com/example/owner/androidtest/ServantStats.java
+++ app/src/main/java/com/example/owner/androidtest/ServantStats.java
@@ -1,5 +1,7 @@
 package com.example.owner.androidtest;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +11,7 @@ import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -18,16 +21,18 @@ import java.io.IOException;
 
 public class ServantStats extends AppCompatActivity
 {
-
+    Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_servant_stats);
         final EditText ET = (EditText) findViewById(R.id.editText);
         ET.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_NUMBER);
         final Button submit = (Button) findViewById(R.id.servantButton);
         final TextView TV = (TextView) findViewById(R.id.textView);
+        final ImageView IV = (ImageView) findViewById(R.id.imageView);
         submit.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -38,6 +43,11 @@ public class ServantStats extends AppCompatActivity
                 int ID = Integer.parseInt(ET.getText().toString());
                 Servant newServant = new Servant(ID);
                 TV.setText(newServant.toString());
+                Portrait pic = new Portrait();
+                pic.setImage(IV);
+                pic.execute(newServant.getImageURL());
+
+
             }
         });
     }
