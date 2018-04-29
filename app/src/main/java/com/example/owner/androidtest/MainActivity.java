23f740ace8fcfdae5404531774c15cf8b6a02ab3
TSCN <franciscoubaldo512@gmail.com>
2018-04-29 09:16:30 -0400

    AsyncTask still recommended on Servant.java, but implemented on portrait.java Basic gacha implemented, display bugs present, working to get progress bar integrated and trying to update textviews inside asynctask thread Gacha 5/4 star list and CE 5 star need a counter, use earlier map java program

--- app/src/main/java/com/example/owner/androidtest/MainActivity.java
+++ app/src/main/java/com/example/owner/androidtest/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ServantStats.class);
+                Intent intent = new Intent(MainActivity.this, Summoning.class); //before was MainActivity.this, ServantStats.class
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/owner/androidtest/Summoning.java
@@ -0,0 +1,62 @@
+package com.example.owner.androidtest;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class Summoning extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_summoning);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        final Button summon10 = (Button) findViewById(R.id.summon10);
+        final TextView TV1 = (TextView) findViewById(R.id.summoning_tv1);
+        final TextView TV2 = (TextView) findViewById(R.id.summoning_tv2);
+        final TextView TV3 = (TextView) findViewById(R.id.summoning_tv3);
+        final ProgressBar progress = (ProgressBar)findViewById(R.id.progressBar);
+        //final ImageView IV = (ImageView) findViewById(R.id.imageView);
+        final Gacha myGacha = new Gacha(progress, TV1, TV2, TV3);
+
+        summon10.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                TV1.setText("");
+                TV2.setText("");
+
+                myGacha.tenPull();
+                myGacha.execute();
+                try {
+                    Thread.sleep(6000);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                TV3.setText(myGacha.getLastResult());
+                for (String value : myGacha.getSSR_Servants())
+                {
+                    TV1.append(value + '\n');
+                }
+                for(String value : myGacha.getSR_Servants())
+                {
+                    TV1.append(value + '\n');
+                }
+                for(String value : myGacha.getSSR_CEs())
+                {
+                    TV2.append(value + '\n');
+                }
+
+            }
+        });
+    }
+
+}
