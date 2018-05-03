5420143ce2de1ec02a4fc3c9c7f0494ca14961f8
TSCN <franciscoubaldo512@gmail.com>
2018-05-03 03:25:30 -0400

    AsyncTask still recommended on Servant.java In the middle of implementing monster spinner still Removed extra toolbar in dmg calculator

--- app/src/main/java/com/example/owner/androidtest/dmg_calculator.java
+++ app/src/main/java/com/example/owner/androidtest/dmg_calculator.java
@@ -14,7 +14,7 @@ public class dmg_calculator extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_dmg_calculator);
         final String[]options = {"Skeleton (Saber)", "Skeleton (Archer)", "Skeleton (Lancer)", "French Soldier (Saber)",
                 "French Soldier (Lancer)", "Roman Soldier (Saber)", "Roman Soldier (Archer)", "Roman Soldier (Lancer)",
@@ -30,142 +30,143 @@ public class dmg_calculator extends AppCompatActivity{
         final EditText IDtaker = (EditText) findViewById(R.id.idTaker);
         final Spinner monsters = (Spinner)findViewById(R.id.monsterSpinner);
         ArrayAdapter<String>adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,options);
-
+        IDtaker.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_NUMBER); //block special characters
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         monsters.setAdapter(adapter);
         monsters.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
             @Override
             public void onItemSelected(AdapterView<?> parent, View v, int position, long id)
             {
-            
-                switch (position) {
+                String monsterClass;
+                switch (position)
+                {
                     case 0:
-                        String monsterClass ="saber";
+                        monsterClass ="saber";
                         break;
                     case 1:
-                        // Whatever you want to happen when the second item gets selected
+                        monsterClass ="archer";
                         break;
                     case 2:
-            
+                        monsterClass="lancer";
                         break;
                     case 3:
-            
+                        monsterClass ="saber";
                         break;
                     case 4:
-            
+                        monsterClass="lancer";
                         break;
                     case 5:
-            
+                        monsterClass ="saber";
                         break;
                     case 6:
-            
+                        monsterClass ="archer";
                         break;
                     case 7:
-            
+                        monsterClass="lancer";
                         break;
                     case 8:
-            
+                        monsterClass ="saber";
                         break;
                     case 9:
-            
+                        monsterClass ="archer";
                         break;
                     case 10:
-            
+                        monsterClass="berserker";
                         break;
                     case 11:
-            
+                        monsterClass="caster";
                         break;
                     case 12:
-            
+                        monsterClass ="saber";
                         break;
                     case 13:
-            
+                        monsterClass ="archer";
                         break;
                     case 14:
-            
+                        monsterClass="lancer";
                         break;
                     case 15:
-            
+                        monsterClass ="saber";
                         break;
                     case 16:
-            
+                        monsterClass ="archer";
                         break;
                     case 17:
-            
+                        monsterClass="lancer";
                         break;
                     case 18:
-            
+                        monsterClass="assassin";
                         break;
                     case 19:
-            
+                        monsterClass="rider";
                         break;
                     case 20:
-            
+                        monsterClass="rider";
                         break;
                     case 21:
-            
+                        monsterClass ="saber";
                         break;
                     case 22:
-            
+                        monsterClass="lancer";
                         break;
                     case 23:
-            
+                        monsterClass="assassin";
                         break;
                     case 24:
-            
+                        monsterClass="rider";
                         break;
                     case 25:
-            
+                        monsterClass="berserker";
                         break;
                     case 26:
-            
+                        monsterClass="caster";
                         break;
                     case 27:
-            
+                        monsterClass="lancer";
                         break;
                     case 28:
-            
+                        monsterClass="berserker";
                         break;
                     case 29:
-            
+                        monsterClass="assassin";
                         break;
                     case 30:
-            
+                        monsterClass="saber";
                         break;
                     case 31:
-            
+                        monsterClass ="archer";
                         break;
                     case 32:
-            
+                        monsterClass="caster";
                         break;
                     case 33:
-            
+                        monsterClass="assassin";
                         break;
                     case 34:
-            
+                        monsterClass ="archer";
                         break;
                     case 35:
-            
+                        monsterClass="lancer";
                         break;
                     case 36:
-            
+                        monsterClass ="saber";
                         break;
                     case 37:
-            
+                        monsterClass="assassin";
                         break;
                     case 38:
-            
+                        monsterClass="assassin";
                         break;
                     case 39:
-            
+                        monsterClass="assassin";
                         break;
                     case 40:
-            
+                        monsterClass="rider";
                         break;
                     case 41:
-            
+                        monsterClass ="archer";
                         break;
-            
+
                 }
             }
             @Override
@@ -173,7 +174,7 @@ public class dmg_calculator extends AppCompatActivity{
             {
             }
         });
-        IDtaker.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_NUMBER); //block special characters
+
         tester.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
