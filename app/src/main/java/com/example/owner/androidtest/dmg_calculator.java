5631e697b28d54e15117e221da3303051cc30e51
TSCN <franciscoubaldo512@gmail.com>
2018-05-03 02:45:21 -0400

    AsyncTask still recommended on Servant.java In the middle of implementing monster spinner still

--- app/src/main/java/com/example/owner/androidtest/dmg_calculator.java
+++ app/src/main/java/com/example/owner/androidtest/dmg_calculator.java
@@ -9,7 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.*;
 
-public class dmg_calculator extends AppCompatActivity {
+public class dmg_calculator extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +29,150 @@ public class dmg_calculator extends AppCompatActivity {
         final ImageView classPic = (ImageView) findViewById(R.id.class_icon);
         final EditText IDtaker = (EditText) findViewById(R.id.idTaker);
         final Spinner monsters = (Spinner)findViewById(R.id.monsterSpinner);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,options);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        monsters.setAdapter(adapter);
+        monsters.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View v, int position, long id)
+            {
+            
+                switch (position) {
+                    case 0:
+                        String monsterClass ="saber";
+                        break;
+                    case 1:
+                        // Whatever you want to happen when the second item gets selected
+                        break;
+                    case 2:
+            
+                        break;
+                    case 3:
+            
+                        break;
+                    case 4:
+            
+                        break;
+                    case 5:
+            
+                        break;
+                    case 6:
+            
+                        break;
+                    case 7:
+            
+                        break;
+                    case 8:
+            
+                        break;
+                    case 9:
+            
+                        break;
+                    case 10:
+            
+                        break;
+                    case 11:
+            
+                        break;
+                    case 12:
+            
+                        break;
+                    case 13:
+            
+                        break;
+                    case 14:
+            
+                        break;
+                    case 15:
+            
+                        break;
+                    case 16:
+            
+                        break;
+                    case 17:
+            
+                        break;
+                    case 18:
+            
+                        break;
+                    case 19:
+            
+                        break;
+                    case 20:
+            
+                        break;
+                    case 21:
+            
+                        break;
+                    case 22:
+            
+                        break;
+                    case 23:
+            
+                        break;
+                    case 24:
+            
+                        break;
+                    case 25:
+            
+                        break;
+                    case 26:
+            
+                        break;
+                    case 27:
+            
+                        break;
+                    case 28:
+            
+                        break;
+                    case 29:
+            
+                        break;
+                    case 30:
+            
+                        break;
+                    case 31:
+            
+                        break;
+                    case 32:
+            
+                        break;
+                    case 33:
+            
+                        break;
+                    case 34:
+            
+                        break;
+                    case 35:
+            
+                        break;
+                    case 36:
+            
+                        break;
+                    case 37:
+            
+                        break;
+                    case 38:
+            
+                        break;
+                    case 39:
+            
+                        break;
+                    case 40:
+            
+                        break;
+                    case 41:
+            
+                        break;
+            
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView)
+            {
+            }
+        });
         IDtaker.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_NUMBER); //block special characters
         tester.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,5 +207,6 @@ public class dmg_calculator extends AppCompatActivity {
             }
         });
     }
+
 
 }
