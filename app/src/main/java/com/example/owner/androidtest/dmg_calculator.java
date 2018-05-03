9e5504483d59234b05dbee08047e15be44524cbc
TSCN <franciscoubaldo512@gmail.com>
2018-05-03 03:55:11 -0400

    AsyncTask still recommended on Servant.java In the middle of implementing monster spinner still Added monster imgUrls

--- app/src/main/java/com/example/owner/androidtest/dmg_calculator.java
+++ app/src/main/java/com/example/owner/androidtest/dmg_calculator.java
@@ -29,6 +29,9 @@ public class dmg_calculator extends AppCompatActivity{
         final ImageView classPic = (ImageView) findViewById(R.id.class_icon);
         final EditText IDtaker = (EditText) findViewById(R.id.idTaker);
         final Spinner monsters = (Spinner)findViewById(R.id.monsterSpinner);
+
+
+
         ArrayAdapter<String>adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,options);
         IDtaker.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_NUMBER); //block special characters
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -37,134 +40,177 @@ public class dmg_calculator extends AppCompatActivity{
             @Override
             public void onItemSelected(AdapterView<?> parent, View v, int position, long id)
             {
-                String monsterClass;
+                String monsterClass ="";
+                String urlImg = "";
                 switch (position)
                 {
                     case 0:
                         monsterClass ="saber";
+                        urlImg = "icons/enemy/enemy_009-01.png";
                         break;
                     case 1:
                         monsterClass ="archer";
+                        urlImg = "icons/enemy/enemy_009-03.png";
                         break;
                     case 2:
                         monsterClass="lancer";
+                        urlImg = "icons/enemy/enemy_009-02.png";
                         break;
                     case 3:
                         monsterClass ="saber";
+                        urlImg = "icons/enemy/enemy_011-01.png";
                         break;
                     case 4:
                         monsterClass="lancer";
+                        urlImg = "icons/enemy/enemy_011-02.png";
                         break;
                     case 5:
                         monsterClass ="saber";
+                        urlImg = "icons/enemy/enemy_019-01.png";
                         break;
                     case 6:
                         monsterClass ="archer";
+                        urlImg = "icons/enemy/enemy_019-03.png";
                         break;
                     case 7:
                         monsterClass="lancer";
+                        urlImg = "icons/enemy/enemy_019-02.png";
                         break;
                     case 8:
                         monsterClass ="saber";
+                        urlImg = "icons/enemy/enemy_037-01.png";
                         break;
                     case 9:
                         monsterClass ="archer";
+                        urlImg = "icons/enemy/enemy_037-03.png";
                         break;
                     case 10:
                         monsterClass="berserker";
+                        urlImg = "icons/enemy/enemy_037-07.png";
                         break;
                     case 11:
                         monsterClass="caster";
+                        urlImg = "icons/enemy/enemy_089-05.png";
                         break;
                     case 12:
                         monsterClass ="saber";
+                        urlImg = "icons/enemy/enemy_089-01.png";
                         break;
                     case 13:
                         monsterClass ="archer";
+                        urlImg = "icons/enemy/enemy_089-03.png";
                         break;
                     case 14:
                         monsterClass="lancer";
+                        urlImg = "icons/enemy/enemy_089-02.png";
                         break;
                     case 15:
                         monsterClass ="saber";
+                        urlImg = "icons/enemy/enemy_044.png";
                         break;
                     case 16:
                         monsterClass ="archer";
+                        urlImg = "icons/enemy/enemy_044.png";
                         break;
                     case 17:
                         monsterClass="lancer";
+                        urlImg = "icons/enemy/enemy_044.png";
                         break;
                     case 18:
                         monsterClass="assassin";
+                        urlImg = "icons/enemy/enemy_022.png";
                         break;
                     case 19:
                         monsterClass="rider";
+                        urlImg = "icons/enemy/enemy_013.png";
                         break;
                     case 20:
                         monsterClass="rider";
+                        urlImg = "icons/enemy/enemy_039.png";
                         break;
                     case 21:
                         monsterClass ="saber";
+                        urlImg = "icons/enemy/enemy_036.png";
                         break;
                     case 22:
                         monsterClass="lancer";
+                        urlImg = "icons/enemy/enemy_036.png";
                         break;
                     case 23:
                         monsterClass="assassin";
+                        urlImg = "icons/enemy/enemy_036.png";
                         break;
                     case 24:
                         monsterClass="rider";
+                        urlImg = "icons/enemy/enemy_062.png";
                         break;
                     case 25:
                         monsterClass="berserker";
+                        urlImg = "icons/enemy/enemy_030.png";
                         break;
                     case 26:
                         monsterClass="caster";
+                        urlImg = "icons/enemy/enemy_026.png";
                         break;
                     case 27:
                         monsterClass="lancer";
+                        urlImg = "icons/enemy/enemy_041.png";
                         break;
                     case 28:
                         monsterClass="berserker";
+                        urlImg = "icons/enemy/enemy_027.png";
                         break;
                     case 29:
                         monsterClass="assassin";
+                        urlImg = "icons/enemy/enemy_079.png";
                         break;
                     case 30:
                         monsterClass="saber";
+                        urlImg = "icons/enemy/enemy_082.png";
                         break;
                     case 31:
                         monsterClass ="archer";
+                        urlImg = "icons/enemy/enemy_086.png";
                         break;
                     case 32:
                         monsterClass="caster";
+                        urlImg = "icons/enemy/enemy_073.png";
                         break;
                     case 33:
                         monsterClass="assassin";
+                        urlImg = "icons/enemy/enemy_090.png";
                         break;
                     case 34:
                         monsterClass ="archer";
+                        urlImg = "icons/enemy/enemy_091.png";
                         break;
                     case 35:
                         monsterClass="lancer";
+                        urlImg = "icons/enemy/enemy_092.png";
                         break;
                     case 36:
                         monsterClass ="saber";
+                        urlImg = "icons/enemy/enemy_093.png";
                         break;
                     case 37:
                         monsterClass="assassin";
+                        urlImg = "icons/enemy/enemy_106.png";
                         break;
                     case 38:
                         monsterClass="assassin";
+                        urlImg = "icons/enemy/enemy_107.png";
                         break;
                     case 39:
                         monsterClass="assassin";
+                        urlImg = "icons/enemy/enemy_108.png";
                         break;
                     case 40:
                         monsterClass="rider";
+                        urlImg = "icons/enemy/enemy_115.png";
                         break;
                     case 41:
                         monsterClass ="archer";
+                        urlImg = "icons/enemy/enemy_118.png";
                         break;
 
                 }
@@ -198,7 +244,7 @@ public class dmg_calculator extends AppCompatActivity{
                         pic.setImage(servantPic);
                         pic.execute(newServant.getImageURL());
                         //int drawableLocation = getResources().getIdentifier("drawable/" + newServant.classIcon(), null, null);
-                        Log.e("IMPORTANTE", newServant.getServantClass());
+                        //Log.e("IMPORTANTE", newServant.getServantClass());
                         int drawableLocation = dmg_calculator.this.getResources().getIdentifier(newServant.classIcon(), "drawable", dmg_calculator.this.getPackageName());
                         //int drawableLocation = getResources().getIdentifier(newServant.classIcon(), "drawable", getPackageName());
                         classPic.setImageResource(drawableLocation);
@@ -208,6 +254,7 @@ public class dmg_calculator extends AppCompatActivity{
             }
         });
     }
+    //public void
 
 
 }
